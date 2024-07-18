d811646deee59bdd3ebec12955f38780a6da80e0
sanya-serduk <sanya.serduk@yandex.ru>
2024-07-18 10:21:08 +0300

    'deploy'

---
+++ assets/StepImage-D9PGZhKF.js
@@ -0,0 +1,1 @@
+import{o as t,c as a,a as s}from"./index-2M_2HP-T.js";const c={class:"step-image"},r=["src","alt"],n={__name:"StepImage",props:{src:{type:String,required:!0},alt:{type:String,default:"image"}},setup(e){return(i,o)=>(t(),a("div",c,[s("img",{class:"step-image__item",src:e.src,alt:e.alt},null,8,r)]))}};export{n as _};
